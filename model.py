5565e0d22312a3a0139e5be96ca55ce87847584d
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-04-07 02:37:44 +0530

    modified model of the database

--- model.py
+++ model.py
@@ -1,29 +1,29 @@
 
-from sqlalchemy import Column, Integer, String, ForeignKey, BigInteger
+from sqlalchemy import Column, Integer, Text, ForeignKey, BigInteger, UnicodeText
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 
 
 Base = declarative_base()
 
-class  Mail(Base):
-    __tablename__ = 'mail'
+class  MailTable(Base):
+    __tablename__ = 'mailTable'
     id = Column(Integer, primary_key=True)
     mail_time = Column(BigInteger)
-    mail_from = Column(String)
-    mail_to = Column(String)
-    subject = Column(String)
-    text_of_body = Column(String)
+    mail_from = Column(Text)
+    mail_to = Column(Text)
+    subject = Column(Text)
+    text_of_body = Column(UnicodeText)
 
 
-class Lable(Base):
+class Label(Base):
     __tablename__ = 'lable'
     id = Column(Integer, primary_key=True)
-    mail_label= Column(String(250), nullable=False)
-    mail_id = Column(Integer, ForeignKey('mail.id'))
-    mail = relationship(Mail)
+    mail_label= Column(UnicodeText(250), nullable=False)
+    mail_id = Column(Integer, ForeignKey('mailTable.id'))
+    mail = relationship(MailTable)
 
 
 engine = create_engine('sqlite:///mails.db')
